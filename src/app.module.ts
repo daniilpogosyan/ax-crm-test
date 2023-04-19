9c519c7fa0589755101584de3ae59d6ba78ee7aa
Daniil Pogosyan <daniilpogosyan@gmail.com>
2023-04-19 09:39:04 +0500

    Chore: Create modules boilerplates

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CallModule } from './modules/call/call.module';
+import { LeadModule } from './modules/lead/lead.module';
+import { ChatModule } from './modules/chat/chat.module';
+import { PersonService } from './modules/person/person.service';
+import { PersonModule } from './modules/person/person.module';
 
 @Module({
-  imports: [],
+  imports: [CallModule, LeadModule, ChatModule, PersonModule],
   controllers: [AppController],
   providers: [AppService],
 })
